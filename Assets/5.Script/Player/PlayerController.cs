678340a8cc130da7431d84848fa94409f61d3c3f
Ku-dodo <rnehgus9403@gmail.com>
2023-12-28 22:20:17 +0900

    [fix] Fix jump logic
    +박스캐스트를 지상에 쏴서 착지 여부 확인
    +공중에서 더 많은 중력을 받도록 함

--- Assets/5.Script/Player/PlayerController.cs
+++ Assets/5.Script/Player/PlayerController.cs
@@ -1,62 +1,104 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Physics")]
     public float moveSpeed;
     public float jumpForce;
+    public float groundGravityScale;
+    public float airGravityScale;
+
+    [Header("Ray to Cheak Layer")]
+    public LayerMask groundLayer;
 
     private Vector2 _curMoveInput;
     private Rigidbody2D _rigidbody2D;
 
     private bool _isGround = false;
+
     #region Unity Flow
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        Physics2D.queriesStartInColliders = false;
     }
 
     private void FixedUpdate()
     {
-        move();
+        Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (groundLayer.value == (groundLayer.value | 1 << collision.gameObject.layer))
+        {
+            _rigidbody2D.gravityScale = groundGravityScale;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (groundLayer.value == (groundLayer.value | 1 << collision.gameObject.layer))
+        {
+            _rigidbody2D.gravityScale = airGravityScale;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireCube(transform.position, transform.lossyScale);
+
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, transform.lossyScale, 0.2f, Vector2.down, transform.lossyScale.y / 2 + 0.1f, groundLayer);
+
+        if (hit.collider != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(hit.point, transform.lossyScale);
+        }
     }
     #endregion
 
-    #region InputAction Method
-    private void move()
+    #region  Method
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            _curMoveInput = context.ReadValue<Vector2>();
+            Debug.Log(transform.right);
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            _curMoveInput = Vector2.zero;
+        }
+    }
+
+    private void Move()
     {
         Vector3 dir = transform.right * _curMoveInput.x;
         dir *= moveSpeed;
         _rigidbody2D.AddForce(dir, ForceMode2D.Impulse);
     }
 
-    public void OnMoveInput(InputAction.CallbackContext context)
-    {
-        if(context.phase == InputActionPhase.Performed)
-        {
-            Debug.Log("2");
-            _curMoveInput = context.ReadValue<Vector2>();
-        }
-        else if (context.phase == InputActionPhase.Canceled)
-        {
-            Debug.Log("3");
-            _curMoveInput = Vector2.zero;
-        }
-    }
-
     public void OnJumpInput(InputAction.CallbackContext context)
     {
-        if(context.phase == InputActionPhase.Started)
+        if (context.phase == InputActionPhase.Started)
         {
-            _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if (IsGround())
+            {
+                _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
     }
+
+    private bool IsGround()
+    {
+        if (Physics2D.BoxCast(transform.position, transform.lossyScale, 0.2f, Vector2.down, transform.lossyScale.y / 2 + 0.1f, groundLayer))
+        {
+            return true;
+        }
+        return false;
+    }
     #endregion
-
-
 }
